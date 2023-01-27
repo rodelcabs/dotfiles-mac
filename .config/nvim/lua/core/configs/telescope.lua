4319449a741ddfd7de9b04d923cd21951ffe885c
rods <rodel.cabubas@asurion.com>
2023-01-27 14:28:26 +0800

    exclude dist folder in search - telescope

--- .config/nvim/lua/core/configs/telescope.lua
+++ .config/nvim/lua/core/configs/telescope.lua
@@ -8,7 +8,7 @@ local actions = require "telescope.actions"
 telescope.setup {
   defaults = {
 
-    file_ignore_patterns = { "node_modules" },
+    file_ignore_patterns = { "node_modules", "dist"},
     prompt_prefix = " ",
     selection_caret = " ",
     path_display = { "smart" },
